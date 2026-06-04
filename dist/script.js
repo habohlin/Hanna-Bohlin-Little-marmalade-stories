a500ffe0f4480f0ca3bf4847dbe8c2b407007e80
Hanna Bohlin <ha.bohlin@gmail.com>
2026-06-04 23:44:38 +0100

    Delete dist directory

--- dist/script.js
+++
@@ -1,9 +0,0 @@
-const moreButton = document.getElementById("more-btn");
-
-let clickCounter = 0;
-
-moreButton.addEventListener("click", () => {
-  clickCounter++;
-  const currBox = document.getElementById(`box${clickCounter}`);
-  currBox.classList.remove("hidden");
-})
